2cb8bf85ff2511bc2154fe4403f8097716acef64
EstevanKov <franciscoesteban15rdz@gmail.com>
2024-11-21 18:53:40 -0600

    b

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,40 +1,41 @@
-// app/(tabs)/_layout.tsx
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-import UsersScreen from '../users/loged';
-import CreateMedicationsScreen from '../medications/create'; 
-import MedicationsScreen from '../medications/index'; 
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import { useColorScheme } from '@/hooks/useColorScheme';
+import 'react-native-reanimated';
+import React from 'react';
 
-const Tab = createBottomTabNavigator();
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
-const getIconName = (routeName: string): keyof typeof Ionicons.glyphMap => {
-  switch (routeName) {
-    case 'medications/index':
-      return 'heart';
-    case 'medications/create':
-      return 'medkit';
-    case 'users/loged':
-      return 'person';
-    default:
-      return 'home'; 
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  const [fontsLoaded] = useFonts({
+    SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
   }
-};
 
-export default function TabLayout() {
   return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => (
-          <Ionicons name={getIconName(route.name)} size={size} color={color} />
-        ),
-        tabBarActiveTintColor: '#2196F3',
-        tabBarInactiveTintColor: 'gray',
-        headerShown: false,
-      })}
-    >
-      <Tab.Screen name="users/loged" component={UsersScreen} options={{ title: '' }} />
-      <Tab.Screen name="medications/index" component={MedicationsScreen} options={{ title: '' }} />
-      <Tab.Screen name="medications/create" component={CreateMedicationsScreen} options={{ title: '' }} />
-    </Tab.Navigator>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="users/loged" />
+        <Stack.Screen name="medications" />
+        <Stack.Screen name="home" />
+        <Stack.Screen name="[...not-found]" options={{ headerShown: true }} />
+      </Stack>
+    </ThemeProvider>
   );
 }
